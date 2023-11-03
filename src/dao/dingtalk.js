7d8edd5aa855f922029788c86b05e4cdf2434505
hwk01056063 <hwk01056063@antgroup.com>
2023-11-03 10:04:21 +0800

    HWK20231103

--- src/dao/dingtalk.js
+++ src/dao/dingtalk.js
@@ -2,9 +2,12 @@ const fetch = require("node-fetch");
 
 // https://open.dingtalk.com/document/group/custom-robot-access
 const dingTalkDao = {
+  // 发送消息
   send: function (groupUrls, content, title, atUid, isAtAll) {
+    // 遍历groupUrls数组
     for (let group of groupUrls) {
       // 1.validate
+      // 判断groupUrls数组是否为空
       if (group == null || group.length == 0) {
         console.log("DingTalk groupUrls is empty");
         continue;
@@ -14,6 +17,7 @@ const dingTalkDao = {
         uidArr = atUid;
       }
       // 2. send request
+      // 发送请求
       fetch(group, {
         method: "POST",
         headers: {
@@ -41,9 +45,12 @@ const dingTalkDao = {
     }
   },
 
+  // 发送markdown消息
   sendMarkdown: function (groupUrls, content, title, atUid, isAtAll) {
+    // 遍历groupUrls数组
     for (let group of groupUrls) {
       // 1.validate
+      // 判断groupUrls数组是否为空
       if (group == null || group.length == 0) {
         console.log("DingTalk groupUrls is empty");
         continue;
@@ -52,33 +59,48 @@ const dingTalkDao = {
       if (atUid != null) {
         uidArr = atUid;
       }
-      // 2. send request
-      fetch(group, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Accept: "application/json",
+
+      console.log(123, JSON.stringify({
+        msgtype: "markdown",
+        markdown: {
+          title: `${title}`,
+          text: content,
         },
-        body: JSON.stringify({
-          msgtype: "markdown",
-          markdown: {
-            title: `${title}`,
-            text: content,
-          },
-          at: {
-            atMobiles: [""],
-            atUserIds: uidArr,
-            isAtAll: isAtAll,
-          },
-        }),
-      })
-        // 3. parse
-        .then((res) => {
-          return res.json();
-        })
-        .then((json) => console.log(json));
+        at: {
+          atMobiles: [""],
+          atUserIds: uidArr,
+          isAtAll: isAtAll,
+        },
+      }))
+      // 2. send request
+      // 发送请求
+        // fetch(group, {
+        //   method: "POST",
+        //   headers: {
+        //     "Content-Type": "application/json",
+        //     Accept: "application/json",
+        //   },
+        //   body: JSON.stringify({
+        //     msgtype: "markdown",
+        //     markdown: {
+        //       title: `${title}`,
+        //       text: content,
+        //     },
+        //     at: {
+        //       atMobiles: [""],
+        //       atUserIds: uidArr,
+        //       isAtAll: isAtAll,
+        //     },
+        //   }),
+        // })
+        //   // 3. parse
+        //   .then((res) => {
+        //     return res.json();
+        //   })
+        //   .then((json) => console.log(json));
     }
   },
+  // 将null转换为空字符串
   nullToEmpty(str) {
     if (str == null) {
       return "";
@@ -88,10 +110,14 @@ const dingTalkDao = {
 };
 
 module.exports = {
+  // 发送消息
   send: function (groupUrls, content, title, atUid, isAtAll) {
+    // 调用dingTalkDao的send方法，发送消息
     return dingTalkDao.send(groupUrls, content, title, atUid, isAtAll);
   },
+  // 发送markdown消息
   sendMarkdown: function (groupUrls, content, title, atUid, isAtAll) {
+    // 调用dingTalkDao的sendMarkdown方法，发送markdown消息
     return dingTalkDao.sendMarkdown(groupUrls, content, title, atUid, isAtAll);
   },
 };
